7abffe143009a7a9d91a684fa03e310df1126c4f
tariq786 <tariqbashir@gmail.com>
2015-11-04 14:54:10 +0000

    first edit

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,9 @@
+import json
 # example of program that calculates the number of tweets cleaned
+
+def tweets_clean_func(filename):
+    f=open(filename)
+    for line in f:
+        print line
+        parsed_line =  json.load(line)
+    return parsed_line
