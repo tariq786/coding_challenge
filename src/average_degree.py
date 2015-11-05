bedc7a67c99ed129f1491c3c7fcc0cd4c21e6cf7
tariq786 <tariqbashir@gmail.com>
2015-11-05 05:54:09 +0000

    working multi dictionary for average_degree.py

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,1 +1,57 @@
 # example of program that calculates the average degree of hashtags
+
+import re
+import sys
+import datetime
+from collections import  defaultdict
+import tweets_cleaned
+
+#input_filename = sys.argv[1]
+#output_filename = sys.argv[2]
+
+#f1   = open(input_filename)
+#fw  = open(output_filename,"w")
+
+def graph_func(input_filename,temp_filename,output_filename):
+    #Calling 1st script
+    tweets_cleaned.clean_func(input_filename,temp_filename)
+
+    f = open(temp_filename)
+    f2 = open("hash_tags.txt","w")
+    for line in f:
+        hash_tag = re.findall(r"#[0-9a-zA-Z.-]+", line)
+        hash_tag = [i.lower() for i in hash_tag]
+        if hash_tag != []:
+            match = re.search(r'\d{2}:\d{2}:\d{2}', line)
+            var_time=datetime.datetime.strptime(match.group(), '%H:%M:%S').time()
+            #print var_time.hour, var_time.minute,var_time.second
+            for item in hash_tag:
+                f2.write(item)
+                f2.write(" ")
+            f2.write(str(var_time.hour))
+            f2.write(" ")
+            f2.write(str(var_time.minute))
+            f2.write(" ")
+            f2.write(str(var_time.second))
+            f2.write("\n")
+    f2.close()
+
+    #get Hashtags with timestamp
+    node_dict = defaultdict(list)
+    f3 = open("hash_tags.txt")
+    for line in f3:
+        tag_time_list=line.split(" ")
+        nodes_list = tag_time_list[1:-3]
+        hour,minutes,second=tag_time_list[-3:]
+        for i,item in enumerate(nodes_list):
+            for node in nodes_list:
+                if(nodes_list[i] != node):
+                    node_dict[nodes_list[i]].append(node)
+        print node_dict
+
+    #create twitter graph
+
+
+if __name__ == '__main__':
+    graph_func(sys.argv[1],sys.argv[2],sys.argv[3])
+
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -3,49 +3,54 @@
 import re
 import sys
 
-counter_unicode =  0
 
-input_filename=sys.argv[1]
-output_filename =  sys.argv[2]
+#input_filename  =  sys.argv[1]
+#output_filename =  sys.argv[2]
 
-fw=open(output_filename,'w')
-f=open(input_filename)
+def clean_func(input_filename,output_filename):
+    fw = open(output_filename,'w')
+    f  = open(input_filename)
+    counter_unicode =  0
 
-for i,line in enumerate(f,1):
-    line_lst = line.split(',"',4)
-    #print line_lst
-    time_stamp = line_lst[0]
-    #print time_stamp
-    time_stamp_val = time_stamp.split(":",1)[1]
-    try:
-        text_field = line_lst[3]
-        #print text_field
-        text_val = text_field.split(":",1)[1]
-        #print text_val
-        if "\u" in text_val:
-            text_val = text_val.decode('unicode_escape').encode('ascii','ignore')
-            counter_unicode += 1
-        if '\/' in text_val:
-            text_val = text_val.replace("\/","/")
+    for i,line in enumerate(f,1):
+        line_lst = line.split(',"',4)
+        #print line_lst
+        time_stamp = line_lst[0]
+        #print time_stamp
+        time_stamp_val = time_stamp.split(":",1)[1]
+        try:
+            text_field = line_lst[3]
+            #print text_field
+            text_val = text_field.split(":",1)[1]
+            #print text_val
+            if "\u" in text_val:
+                text_val = text_val.decode('unicode_escape').encode('ascii','ignore')
+                counter_unicode += 1
+            if '\/' in text_val:
+                text_val = text_val.replace("\/","/")
 
-        text_val = re.sub(' +',' ',text_val)
-        text_val = re.sub('\n+',' ',text_val)
-        text_val = re.sub('\t+',' ',text_val)
-        #print text_val
-        if  text_val != '""':
-            fw.write(text_val[1:-1])
-            fw.write(" ")
-            fw.write("(timestamp: ")
-            fw.write(time_stamp_val[1:-1])
-            fw.write(")")
-            fw.write("\n")
+            text_val = re.sub(' +',' ',text_val)
+            text_val = re.sub('\n+',' ',text_val)
+            text_val = re.sub('\t+',' ',text_val)
+            #print text_val
+            if  text_val != '""':
+                fw.write(text_val[1:-1])
+                fw.write(" ")
+                fw.write("(timestamp: ")
+                fw.write(time_stamp_val[1:-1])
+                fw.write(")")
+                fw.write("\n")
 
-    except IndexError:
-        #print "Invalid Line detected on Line %i" %(i)
-        pass
+        except IndexError:
+            #print "Invalid Line detected on Line %i" %(i)
+            pass
 
-fw.write("\n")
-fw.write("%d tweets contained unicode." %(counter_unicode))
-fw.close()
+    fw.write("\n")
+    fw.write("%d tweets contained unicode." %(counter_unicode))
+    fw.close()
 
-#    return time_stamp, text_val, counter_unicode
+    return True
+
+
+if __name__ == '__main__':
+    clean_func(sys.argv[1],sys.argv[2])
